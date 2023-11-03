43a61eae80c183c2a02f500977ab92f83de7ef38
rajarul82 <a.rajarul@outlook.com>
2023-11-03 12:38:18 +0530

    initial

---
+++ readSecret/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+
+namespace readSecret.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly SecretClient _secretClient;
+
+        public WeatherForecastController(SecretClient secretClient, ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            _secretClient = secretClient;
+        }
+
+        [HttpGet]
+        public async void ReadSecreKey()
+        {
+            var secretValue = await _secretClient.GetSecretAsync("myKey");
+            _logger.LogInformation($"secret value - {secretValue.Value.Value}");
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ readSecret/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Azure;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, config) =>
+{
+    config.ReadFrom.Configuration(context.Configuration);
+});
+// Add services to the container.
+builder.Services.AddAzureClients(azureFactory =>
+{
+    azureFactory.AddSecretClient(builder.Configuration.GetSection("KeyVault"));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
